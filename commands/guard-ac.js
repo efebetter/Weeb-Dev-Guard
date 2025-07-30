fbf1c4be7f21395527a7ebad76e2bf642be27da3
efebetterdev <sendearaz@gmail.com>
2025-07-30 16:05:34 +0300

    Update guard-ac.js

--- commands/guard-ac.js
+++ commands/guard-ac.js
@@ -2,26 +2,26 @@ const Discord = require("discord.js");
 const db = require("croxydb");
 
 module.exports = {
-    name: "guard-aç",
-    description: "Guard sistemini açar!",
+    name: "enable-guard",
+    description: "Enables the guard system!",
     type: 1,
     run: async (client, interaction) => {
-        const yetki = new Discord.EmbedBuilder()
+        const noPermission = new Discord.EmbedBuilder()
             .setColor("Red")
-            .setTitle("Yetersiz Yetki!")
-            .setDescription("> Bu komutu kullanabilmek için `Sunucu Sahibi` olmalısın!");
+            .setTitle("Insufficient Permission!")
+            .setDescription("> You must be the **Server Owner** to use this command!");
 
         if (interaction.guild.ownerId !== interaction.user.id) {
-            return interaction.reply({ embeds: [yetki], ephemeral: true });
+            return interaction.reply({ embeds: [noPermission], ephemeral: true });
         }
 
         db.set(`guard_${interaction.guild.id}`, true);
 
-        const basarili = new Discord.EmbedBuilder()
+        const success = new Discord.EmbedBuilder()
             .setColor("Green")
-            .setTitle("Guard Sistemi Açıldı!")
-            .setDescription("Guard sistemi başarıyla açıldı!");
+            .setTitle("Guard System Enabled!")
+            .setDescription("The guard system has been successfully enabled!");
 
-        interaction.reply({ embeds: [basarili] });
+        interaction.reply({ embeds: [success] });
     }
 }
